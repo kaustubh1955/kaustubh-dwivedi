46054bb5246d937786a776cf696f438407a5c8bf
kaustubh1955 <kaustubh1955@gmail.com>
2021-08-13 17:24:47 +0530

    Reload attempt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Main from './layouts/Main'; // fallback for lazy pages
-import './static/css/main.scss'; // All of our styles
+import React, { lazy, Suspense } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Main from "./layouts/Main"; // fallback for lazy pages
+import "./static/css/main.scss"; // All of our styles
 
 const { PUBLIC_URL } = process.env;
 
 // Every route - we lazy load so that each page can be chunked
 // NOTE that some of these chunks are very small. We should optimize
 // which pages are lazy loaded in the future.
-const About = lazy(() => import('./pages/About'));
-const Contact = lazy(() => import('./pages/Contact'));
-const Index = lazy(() => import('./pages/Index'));
-const NotFound = lazy(() => import('./pages/NotFound'));
-const Projects = lazy(() => import('./pages/Projects'));
-const Resume = lazy(() => import('./pages/Resume'));
-const Stats = lazy(() => import('./pages/Stats'));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Index = lazy(() => import("./pages/Index"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Resume = lazy(() => import("./pages/Resume"));
+const Stats = lazy(() => import("./pages/Stats"));
 
 const App = () => (
-  <BrowserRouter basename={PUBLIC_URL}>
+  <HashRouter basename={PUBLIC_URL}>
     <Suspense fallback={<Main />}>
       <Switch>
         <Route exact path="/" component={Index} />
@@ -29,7 +29,7 @@ const App = () => (
         <Route component={NotFound} status={404} />
       </Switch>
     </Suspense>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
