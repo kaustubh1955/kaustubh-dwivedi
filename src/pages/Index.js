a1024f7398b56598aadf5142135df7639e172dcd
kaustubh1955 <kaustubh1955@gmail.com>
2021-09-05 20:09:13 +0530

    Section removed

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,20 +9,9 @@ const Index = () => (
     }
   >
     <article className="post" id="index">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading">
-            <Link to="/">About this site</Link>
-          </h2>
-          <p>
-            A beautiful, responsive, react application written with modern
-            Javascript.
-          </p>
-        </div>
-      </header>
       <p>
         {" "}
-        Welcome to my website. Please feel free to read more{" "}
+        Hi there! Welcome to my website. Please feel free to read more{" "}
         <Link to="/about">about me</Link>, or you can check out my{" "}
         <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{" "}
         or <Link to="/contact">contact</Link> me.
